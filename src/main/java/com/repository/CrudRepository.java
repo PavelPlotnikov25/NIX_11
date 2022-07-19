e7fb8f2d65e20c3b40998eedfdc72690b4ef6c8f
PavelPlotnikov <ppasha81@gmail.com>
2022-07-19 09:45:52 +0300

    HW_Generics

--- src/main/java/com/repository/CrudRepository.java
+++ src/main/java/com/repository/CrudRepository.java
@@ -1,12 +1,13 @@
 package com.repository;
 
 
+import com.model.Product;
 import com.model.computer.Computer;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface CrudRepository<T> {
+public interface CrudRepository<T extends Product> {
     Computer save(T product);
 
     void saveAll(List<T> product);
